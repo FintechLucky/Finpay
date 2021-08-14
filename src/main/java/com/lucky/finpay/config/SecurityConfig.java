13d2fa57a8f02a038832af32041c1632f51d2aef
yuseogi0218 <yuseogi0218@gmail.com>
2021-08-14 20:56:20 +0900

    kakaoApiLogin&Friends

---
+++ src/main/java/com/lucky/finpay/config/SecurityConfig.java
@@ -0,0 +1,34 @@
+package com.lucky.finpay.config;
+
+import com.lucky.finpay.service.KakaoOAuth2UserService;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private final KakaoOAuth2UserService kakaoOAuth2UserService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                    .httpBasic()
+                .and()
+                    .authorizeRequests()
+                    .antMatchers("/user/**").hasRole("USER")
+                    .antMatchers("/**").permitAll()
+                .and()
+                    .logout()
+                        .logoutUrl("/user/logout.do")
+                        .logoutSuccessUrl("/")
+                        .invalidateHttpSession(true)
+                .and()
+                    .oauth2Login().userInfoEndpoint().userService(kakaoOAuth2UserService);
+    }
+
+}
